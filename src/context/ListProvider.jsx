74e33464caff500e86b6120cfb1221d5c09d93f2
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-12 15:45:49 -0700

    added to list reducer the add item and context on listprovider

--- src/context/ListProvider.jsx
+++ src/context/ListProvider.jsx
@@ -1,16 +1,39 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
+const initialList = [{ text: 'Milk' }];
 
+const listReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_ITEM':
+            return [{ id: Date.now(), text: action.payload.text }, ...state];
 
+           
+    }
+}
 
 
 
 const ListContext = createContext();
 export const ListProvider = ({ children }) => {
-    
+    const [list, setList] = useReducer(listReducer, initialList);
+
+    const handleAddList = (text) => {
+        dispatch({ type: 'ADD_ITEM', payload: { text } });
+    };
 
     
 
   return (
+      <ListContext.Provider
+      value={{ list, setList, handleAddList }}>
+          {children}
+      </ListContext.Provider>
     
-  )
-}
+  );
+};
+export const useLists = () => {
+    const context = useContext(ListContext);
+
+    if (context === undefined)
+    throw new Error('useList must be called from ListProvider');
+    return context;
+};
